7ef822289e79ff97c1791f91f3238f8c9c153cd9
stefKiesz <bluejay49er@gmail.com>
2020-03-09 12:26:39 -0500

    Renewing code for Multi.asm. I had error on the multi.hack.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -11,49 +11,84 @@
 //Setting the argument
 
 //STEP 1
-@R2
-M=0
+//@R2
+//M=0
 
 //STEP 2
-@counter // M = counter
+//@counter // M = counter
 
 //STEP 3
-(LOOP)
+//(LOOP)
 
 //STEP 4
-@counter
-D=M // D = counter
+//@counter
+//D=M // D = counter
 
 //STEP 5
-@R1
-D=D-M // D = counter-M
+//@R1
+//D=D-M // D = counter-M
 
 //STEP 6
-@END
-D;JGE // if counter >= 0 or if R1 >= END
+//@END
+//D;JGE // if counter >= 0 or if R1 >= END
 
 ////////////////////////////////////////////////////
 
 //STEP 7
-@counter
-M=M+1 // counter++
-@R0
-D=M // D=R0
+//@counter
+//M=M+1 // counter++
+//@R0
+//D=M // D=R0
 
 //STEP 8
-@R2
-M=M+D // R2=R2+R0
+//@R2
+//M=M+D // R2=R2+R0
 
 //STEP 9
-@LOOP
+//@LOOP
 
 //STEP 10
-0;JMP
+//0;JMP
 
+//(END)
+//@END
+
+//0;JMP
+
+@i // refers to some memory location
+M=0 // i=0
+
+@result // refers to another location
+M=0
+
+(LOOP)
+        @i
+        D=M // D = i
+        @R1
+        D=D-M // D=i-R1
+        @END
+        D;JEQ // if (i-100)>0 goto END
+
+        @result
+        D=M // D=i
+        @R0
+        D=D+M
+        @result
+        M=D // result=result+b
+
+        @i
+        M=M+1 // i++
+        @LOOP
+        0;JMP // Goto LOOP
+        
 (END)
-@END
+        @result
+        D=M
+        @R2
+        M=D // R2 = result
+        @END
+        0;JMP // Infinite loop
 
-0;JMP
 
 
 
