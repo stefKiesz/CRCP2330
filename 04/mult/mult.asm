d1c0afee9a7194b8ae90d9dbdb92005dbfa05ed2
stefKiesz <bluejay49er@gmail.com>
2020-03-09 12:36:08 -0500

    Working on Multi.asm. I'm initializing i with value in R1 and the product with R0.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -8,86 +8,26 @@
 
 // Put your code here.
 
-//Setting the argument
+//Making a counter
+//Initializing i with value in R1
 
-//STEP 1
-//@R2
-//M=0
+@R1
+D=M
 
-//STEP 2
-//@counter // M = counter
+@i
+M=D
 
-//STEP 3
-//(LOOP)
+//Initilaizing product with 0
+@R0
+D=A
+@product
+M=D
 
-//STEP 4
-//@counter
-//D=M // D = counter
 
-//STEP 5
-//@R1
-//D=D-M // D = counter-M
 
-//STEP 6
-//@END
-//D;JGE // if counter >= 0 or if R1 >= END
 
-////////////////////////////////////////////////////
 
-//STEP 7
-//@counter
-//M=M+1 // counter++
-//@R0
-//D=M // D=R0
 
-//STEP 8
-//@R2
-//M=M+D // R2=R2+R0
-
-//STEP 9
-//@LOOP
-
-//STEP 10
-//0;JMP
-
-//(END)
-//@END
-
-//0;JMP
-
-@i // refers to some memory location
-M=0 // i=0
-
-@result // refers to another location
-M=0
-
-(LOOP)
-        @i
-        D=M // D = i
-        @R1
-        D=D-M // D=i-R1
-        @END
-        D;JEQ // if (i-100)>0 goto END
-
-        @result
-        D=M // D=i
-        @R0
-        D=D+M
-        @result
-        M=D // result=result+b
-
-        @i
-        M=M+1 // i++
-        @LOOP
-        0;JMP // Goto LOOP
-        
-(END)
-        @result
-        D=M
-        @R2
-        M=D // R2 = result
-        @END
-        0;JMP // Infinite loop
 
 
 
