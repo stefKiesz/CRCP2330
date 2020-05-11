b9e5f6f4c918185d275d51cbac893720e6d41270
stefKiesz <bluejay49er@gmail.com>
2020-05-11 14:40:01 -0500

    Working on Assemble. Table.h. Initializing the table for maping address.

---
+++ 06/Assemble/Assemble/Table.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef ASSEMBLER_TABLE_H
+#define ASSEMBLER_TABLE_H
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Table{
+public:
+
+};
+#endif // !ASSEMBLER_TABLE_H
+
+
