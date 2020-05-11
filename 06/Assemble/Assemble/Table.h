1af7e3d4a39671c5de8c2d194768ba0857204952
stefKiesz <bluejay49er@gmail.com>
2020-05-11 15:01:08 -0500

    Working on Assemble. Table.cpp Setting the table with predefined symbols.

--- 06/Assemble/Assemble/Table.h
+++ 06/Assemble/Assemble/Table.h
@@ -8,7 +8,23 @@
 using namespace std;
 
 class Table{
+
 public:
+
+//initializing map
+	Table();
+
+//adding values to table
+	void add(string symbol, int address);
+
+//containing values to table if true
+	bool contains(string symbol);
+
+//returning address
+	int returnAddress(string symbol);
+
+private:
+	map <string, int> AddressTable;
 
 };
 #endif // !ASSEMBLER_TABLE_H
