6e52781679e20c4704e60e0aa0277d68f544be44
stefKiesz <bluejay49er@gmail.com>
2020-05-10 20:55:32 -0500

    Working on Assembler. Main.cpp. I am setting the setup for putting the input and output file names. I am also gonna place the insturctions to use the assembler for user.

---
+++ 06/Assemble/Assemble/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <bitset>
+/*Library Reference: http://www.cplusplus.com/reference/ */
+
+using namespace std;
+
+int main(int info1, char info2[]) {
+
+	string inputFileName, outputFileName;
+	
+	int lineNumberROM, newAddress;
+	
+	unsigned long lineNumberSource;
+	
+	ofstream fout;
+
+
+}
