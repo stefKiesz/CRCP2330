e15349119d2fe78d90d669176dd2a85d59e83d75
stefKiesz <bluejay49er@gmail.com>
2020-05-12 02:45:18 -0500

    Completed Assemble. Finished implemnting the Assembler.cpp and finished the instructions of translating the .asm in Main.cpp. Testing was a success. I placed a README file in the Assemble file in projecy 06 file.

--- 06/Assemble/Assemble/Main.cpp
+++ 06/Assemble/Assemble/Main.cpp
@@ -5,12 +5,13 @@
 
 #include "Parser.h"
 #include "Table.h"
+#include "Assembler.h"
 
 using namespace std;
 
 int main(int infoA, char* infoB[]) {
 
-	//setup
+//setup
 	string inputFileName, outputFileName;
 
 	int lineNumberROM, newAddress;
@@ -19,13 +20,13 @@ int main(int infoA, char* infoB[]) {
 
 	ofstream fout;
 
-	//setting instructions
+//setting instructions
 	if (infoA < 2 || infoA > 3) {
 		cout << "Entry: " << infoB[0] << " <inputfilename.asm> ->" << endl;
 		exit(1);
 	}
 	else {
-		//setting input file
+//setting input file
 		inputFileName = infoB[1];
 		outputFileName = inputFileName.substr(0, inputFileName.length() - 4) + ".hack";
 
@@ -33,16 +34,16 @@ int main(int infoA, char* infoB[]) {
 			outputFileName = infoB[2];
 		}
 	}
-	//setting output file
+//setting output file
 	fout.open(outputFileName);
 
-	//if it didn't go through
+//if it didn't go through
 	if (fout.fail()) {
 		cout << "Could not translate file." << endl;
 		exit(1);
 	}
 
-
+//commands
 	Parser symbolSource(inputFileName);
 	Table symbolTable;
 
@@ -64,4 +65,50 @@ int main(int infoA, char* infoB[]) {
 			symbolTable.add(symbolSource.symbol(), lineNumberROM);
 		}
 	}
+
+//translate
+	Parser assemblySource(inputFileName);
+	Assembler assemble;
+
+	lineNumberSource = 0;   
+	newAddress = 16;      
+
+	while (true) {
+		assemblySource.nextLine(lineNumberSource);
+
+		if (!assemblySource.Commands()) {
+			break;
+		}
+
+		if (assemblySource.commandType(lineNumberSource) == 'A') {
+//A-instructions start with '0'.
+			fout << '0';  
+
+//if symbol is a number.
+			if (assemblySource.symbol().find_first_not_of("0123456789") == string::npos) {
+//string -> decimal # -> binary #.
+				fout << bitset<15>(stoull(assemblySource.symbol(), nullptr)).to_string();
+			}
+			else {
+//if symbol is a variable.
+				if (!symbolTable.contains(assemblySource.symbol())) {
+					symbolTable.add(assemblySource.symbol(), newAddress++);
+				}
+//address -> decimal # -> binary #.
+				fout << bitset<15>(static_cast<unsigned long long>(symbolTable.returnAddress(assemblySource.symbol()))).to_string();
+			}
+			fout << endl;
+		}
+		else if (assemblySource.commandType(lineNumberSource) == 'C') {
+//C-instructions start with "111".
+			fout << "111";  
+			fout << assemble.comp(assemblySource.compP(), lineNumberSource);
+			fout << assemble.dest(assemblySource.destP(), lineNumberSource);
+			fout << assemble.jump(assemblySource.jumpP(), lineNumberSource);
+			fout << endl;
+		}
+	}
+
+	fout.close();
+	return 0;
 }
---
+++ 06/Assemble/Assemble/ReadMe.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//README FILE
+
+//INSTRUCTIONS TO RUN PROGRAM:
+//Go to Assemble file (1)
+//Go to Debug file (2)
+//Drag the program to the commandline (3)
+//Leave 1 space for the .asm program you want to translate (4)
+//Press enter and it should run (5)
+
+//On writing in the commandline:
+//Route to the application of Assemble and type "<inputfilename.asm> ->"
+//It should run. (although dragging it is easier).
